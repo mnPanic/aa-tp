809dc3b654e91374b5d80ef1d8ca893f50dc9cb9
mnPanic <panicmanu@gmail.com>
2022-07-03 15:51:09 -0300

    graficos

--- result_io.py
+++ result_io.py
@@ -4,7 +4,7 @@ import json
 NAME_COLLABORATIVE = "collaborative"
 NAME_CONTENT = "content_based"
 NAME_RANDOM = "random"
-
+NAME_COLLAB_CLUSTER = "collaborative-clustering"
 
 def write_recoms_by_movie(recoms: dict):
     with open("results/recoms-by-movie.json", 'w') as f:
@@ -24,12 +24,16 @@ def write_results(name: str, predicted: List[List[str]], actual: List[List[str]]
         f.write(list_list_to_string(actual)+"\n")
 
 def write_col_result(user_id: int, predicted: list, actual: list):
-    with open(filename(NAME_COLLABORATIVE), 'a') as f:
+    write_results_new_format(NAME_COLLABORATIVE)
+    
+def write_results_new_format(name: str, user_id: int, predicted: list, actual: list):
+    with open(filename(name), 'a') as f:
         actual_str = list_to_string(actual)
         pred_str = list_to_string(predicted)
 
         f.write(f"{user_id}|{pred_str}|{actual_str}\n") 
 
+        
 def read_results(name: str) ->  Tuple[List[List[str]], List[List[str]]]:
     with open(filename(name), 'r') as f:
         content = f.read()
@@ -57,8 +61,8 @@ def read_col_results() -> Tuple[List[List[str]], List[List[str]]]:
         
         return predicted, actual
 
-def read_processed_users() -> List[str]:
-    with open(filename(NAME_COLLABORATIVE), 'r') as f:
+def read_processed_users_for(name: str) -> List[str]:
+    with open(filename(name), 'r') as f:
         content = f.read()
         if content == "":
             print("No processed users")
@@ -67,6 +71,9 @@ def read_processed_users() -> List[str]:
         users = map(lambda l: int(l.split('|')[0]), lines)
 
     return list(users)
+    
+def read_processed_users() -> List[str]:
+    return read_processed_users(NAME_COLLABORATIVE)
     
 def list_list_to_string(ll: List[List[str]]):
     # [["1", "2", "3"], ["4"], ["5"]]
